4cd6eab0c2caf739851e7222035baae41bc4fda2
vincentjimi <vincentjimi50@gmail.com>
2020-12-28 19:45:44 +0100

    initial folder

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+
+
+}); // document ready function
+
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2);
+var scene3 = document.getElementById('scene3');
+var parallaxInstance = new Parallax(scene3);
+var scene4 = document.getElementById('scene4');
+var parallaxInstance = new Parallax(scene4);
+var scene5 = document.getElementById('scene5');
+var parallaxInstance = new Parallax(scene5);
+parallaxInstance.friction(0.2, 0.2);
