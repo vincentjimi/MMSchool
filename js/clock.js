2844c882734da805e7ec602ce6357831f8688f37
vincentjimi <vincentjimi50@gmail.com>
2020-12-31 16:14:56 +0100

    current progres

---
+++ js/clock.js
@@ -0,0 +1,51 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+drawFace(ctx, radius/1.1);
+drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+var grad;
+ctx.beginPath();
+ctx.arc(0, 0, radius, 0, 2*Math.PI);
+ctx.fillStyle = 'white';
+ctx.fill();
+
+}
+
+
+function drawTime(ctx, radius){
+var now = new Date();
+var hour = now.getHours();
+var minute = now.getMinutes();
+var second = now.getSeconds();
+//hour
+hour=hour%12;
+hour=(hour*Math.PI/6)+
+(minute*Math.PI/(6*60))+
+(second*Math.PI/(360*60));
+drawHand(ctx, hour, radius*0.5, radius*0.07);
+//minute
+minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+drawHand(ctx, minute, radius*0.8, radius*0.07);
+// second
+second=(second*Math.PI/30);
+drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+ctx.beginPath();
+ctx.lineWidth = width;
+ctx.lineCap = "round";
+ctx.moveTo(0,0);
+ctx.rotate(pos);
+ctx.lineTo(0, -length);
+ctx.stroke();
+ctx.rotate(-pos);
+}
